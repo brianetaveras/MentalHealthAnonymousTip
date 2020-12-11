42e15d155d44f182136f9fe369ffd96771751f2c
Brian Taveras <taveras574@gmail.com>
2020-12-10 21:16:01 -0500

    removed unused functions

--- api/SMS/SMSHandler.py
+++ api/SMS/SMSHandler.py
@@ -7,18 +7,11 @@ class SMSHandler:
         self.account_sid = account_sid
         self.auth_token = auth_token
         self.twilio_client = None
-        self.queue = deque()
         self.initializeTwilioClient()
-        self.initializeMessageQueue()
 
     def initializeTwilioClient(self):
         self.twilio_client = Client(self.account_sid, self.auth_token)
 
-    def initializeMessageQueue(self):
-        while self.queue:
-            current_message = self.queue.pop()
-            self.sendMessage(current_message)
-            console.log('sent a message')
 
     def sendMessage(self, message):
         message = self.twilio_client.messages.create(
@@ -26,9 +19,5 @@ class SMSHandler:
                     from_= "+19728454955",
                     body= message["body"])
                         
-
-    def addMessageToQueue(self, message):
-        self.queue.append(message)
-        print("Message added to queue")
 
 
