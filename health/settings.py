e1f7da2f176c55e23b86d8800d32ab9da8014ced
Brian Taveras <taveras574@gmail.com>
2020-12-12 19:17:23 -0500

    More changes to static file config

--- health/settings.py
+++ health/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 #DataFlair #Django #Static files
 STATIC_URL = '/static/'
 #--------------------------------------------------
-# STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
 #-----------------------------------------------------
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),]
